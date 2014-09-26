e8b169f0e90eeb5627facf4997ecd3127b48ffb9
Mobius5150 <mike@michaelblouin.ca>
2014-09-25 22:07:37 -0600

    Clean up TodoItemListFragment.
    Reduced responsibilities on the class be creating a private subclass
    `MultiChoiceListener` responsible for handling the selection in the list
    view.

--- src/com/michaelblouin/notes/TodoItemListFragment.java
+++ src/com/michaelblouin/notes/TodoItemListFragment.java
@@ -42,7 +42,7 @@ import com.michaelblouin.todo.TodoItemMailer;
  * in two-pane mode (on tablets) or a {@link TodoItemDetailActivity}
  * on handsets.
  */
-public class TodoItemListFragment extends ListFragment implements MultiChoiceModeListener {
+public class TodoItemListFragment extends ListFragment {
     /**
      * The fragment argument representing the item ID that this fragment
      * represents.
@@ -71,6 +71,106 @@ public class TodoItemListFragment extends ListFragment implements MultiChoiceMod
      * The selected items in the list
      */
     List<TodoItem> selectedItems;
+    
+    private class MultiChoiceListener implements MultiChoiceModeListener {
+    	/**
+         * This method is called when the user clicks on a button in one of the menus.
+         * Typically, the user has selected a set of todo items, and wants to apply an action to them. 
+         */
+    	@Override
+    	public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+    		switch (menuItem.getItemId()) {
+    			case R.id.delete_todoitem:
+    				deleteTodoItems();
+    				break;
+    				
+    			case R.id.email_todoitems:
+    				emailTodoItems();
+    				break;
+    				
+    			case Menu.NONE:
+    				moveTodoItems(menuItem.getTitle().toString());
+    				break;
+    				
+    			default:
+    				return true;	
+    		}
+    		
+    		refreshListView(getListView(), actionMode);
+    		
+    		return true;
+    	}
+    	
+    	@Override
+    	public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+    		MenuInflater inflater = actionMode.getMenuInflater();
+    		inflater.inflate(R.menu.menu_todoitem_context, menu);
+    		
+    		actionMode.setTitle(R.string.action_mode_select_todoitems);
+    		
+    		return true;
+    	}
+
+    	@Override
+    	public void onDestroyActionMode(ActionMode actionMode) {
+    		System.out.println("Action mode destroyed");
+    		
+    		if (selectedItems != null) {
+    			selectedItems = null;
+    		}
+    	}
+
+    	@Override
+    	public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+    		// Add the menu items for each todo group, except the current one
+    		MenuItem addToMenu = menu.findItem(R.id.add_to);
+
+    		if (null == addToMenu) {
+    			return false;
+    		}
+    		
+    		if (!addToMenu.hasSubMenu()) {
+    			return false;
+    		}
+
+    		Menu moveToMenu = addToMenu.getSubMenu();
+
+    		int i = 0;
+    		for (TodoGroup group: mItems) {
+    			if (mItem.getGroupName() == group.getGroupName()) {
+    				continue;
+    			}
+    			
+    			moveToMenu.add(Menu.NONE, Menu.NONE, i, group.getGroupName());
+    			++i;
+    		}
+
+    		return true;
+    	}
+    	
+    	@Override
+    	public void onItemCheckedStateChanged(ActionMode actionMode, int position, long id, boolean checked) {
+    		System.out.println(String.format("Item %d is now %schecked", position, checked ? "": "un"));
+    		
+    		TodoItem item = (TodoItem) getListView().getItemAtPosition(position);
+    		
+    		if (null == item) {
+    			return;
+    		}
+    		
+    		if (null == selectedItems) {
+    			 selectedItems = new ArrayList<TodoItem>();
+    		}
+    		
+    		if (checked) {
+    			if (!selectedItems.contains(item)) {
+    				selectedItems.add(item);
+    			}
+    		} else {
+    			selectedItems.remove(item);
+    		}
+    	}
+    }
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -120,7 +220,7 @@ public class TodoItemListFragment extends ListFragment implements MultiChoiceMod
         
         ListView listView = getListView();
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-        listView.setMultiChoiceModeListener(this);
+        listView.setMultiChoiceModeListener(new MultiChoiceListener());
     }
 
     @Override
@@ -149,79 +249,6 @@ public class TodoItemListFragment extends ListFragment implements MultiChoiceMod
         mActivatedPosition = position;
     }
     
-    /**
-     * This method is called when the user clicks on a button in one of the menus.
-     * Typically, the user has selected a set of todo items, and wants to apply an action to them. 
-     */
-	@Override
-	public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-		switch (menuItem.getItemId()) {
-			case R.id.add_to:
-				System.out.println("Add item to clicked");
-				break;
-				
-			case R.id.delete_todoitem:
-				System.out.println("Delete todo item clicked");
-				List<TodoItem> items = mItem.getItems();
-
-				if (null != selectedItems) {
-					for (TodoItem item: selectedItems) {
-						System.out.println(String.format("Deleting item %s", item.getText()));
-						if (!items.remove(item)) {
-							throw new IllegalStateException("Could not find item to delete in the collection");
-						}
-					}
-
-					selectedItems = null;
-				}
-				
-				refreshListView(getListView(), actionMode);
-				break;
-				
-			case R.id.email_todoitems:
-				System.out.println("Email todogroup button pressed");
-				
-				if (null != selectedItems) {
-					TodoItemMailer mailer = new TodoItemMailer(getActivity());
-					mailer.addTodoItems(mItem.getGroupName(), selectedItems);
-					mailer.send();
-					selectedItems = null;
-				}
-
-				refreshListView(getListView(), actionMode);
-				break;
-				
-			case Menu.NONE:
-				String addToGroupName = menuItem.getTitle().toString();
-				System.out.println(String.format("Move to menu item clicked: %s", addToGroupName));
-				
-				TodoGroup addToGroup = todoGroupProvider.getTodoGroupByName(addToGroupName);
-				
-				if (null == addToGroup) {
-					throw new IllegalStateException("Selected group was not found in the group list");
-				}
-
-				ListView view = getListView();
-
-				if (null != selectedItems) {
-					for (TodoItem item: selectedItems) {
-						System.out.println(String.format("Moving item %s", item.getText()));
-						mItem.moveItemToGroup(item, addToGroup);
-					}
-					
-					selectedItems = null;
-				}
-
-				refreshListView(view, actionMode);
-				break;
-				
-			default:
-				System.out.println("Action item clicked");	
-		}
-		
-		return true;
-	}
-	
 	public void notifyDataSetChanged() {
 		ListView listView = getListView();
 		
@@ -248,73 +275,46 @@ public class TodoItemListFragment extends ListFragment implements MultiChoiceMod
 		}
 	}
 
-	@Override
-	public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-		MenuInflater inflater = actionMode.getMenuInflater();
-		inflater.inflate(R.menu.menu_todoitem_context, menu);
-		
-		actionMode.setTitle(R.string.action_mode_select_todoitems);
-		
-		return true;
-	}
+	private void deleteTodoItems() {
+		List<TodoItem> items = mItem.getItems();
 
-	@Override
-	public void onDestroyActionMode(ActionMode actionMode) {
-		System.out.println("Action mode destroyed");
-		
-		if (selectedItems != null) {
+		if (null != selectedItems) {
+			for (TodoItem item: selectedItems) {
+				System.out.println(String.format("Deleting item %s", item.getText()));
+				if (!items.remove(item)) {
+					throw new IllegalStateException("Could not find item to delete in the collection");
+				}
+			}
+
 			selectedItems = null;
 		}
 	}
-
-	@Override
-	public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-		// Add the menu items for each todo group, except the current one
-		MenuItem addToMenu = menu.findItem(R.id.add_to);
-
-		if (null == addToMenu) {
-			return false;
-		}
+	
+	private void moveTodoItems(String addToGroupName) {
+		System.out.println(String.format("Move to menu item clicked: %s", addToGroupName));
 		
-		if (!addToMenu.hasSubMenu()) {
-			return false;
+		TodoGroup addToGroup = todoGroupProvider.getTodoGroupByName(addToGroupName);
+		
+		if (null == addToGroup) {
+			throw new IllegalStateException("Selected group was not found in the group list");
 		}
 
-		Menu moveToMenu = addToMenu.getSubMenu();
-
-		int i = 0;
-		for (TodoGroup group: mItems) {
-			if (mItem.getGroupName() == group.getGroupName()) {
-				continue;
+		if (null != selectedItems) {
+			for (TodoItem item: selectedItems) {
+				System.out.println(String.format("Moving item %s", item.getText()));
+				mItem.moveItemToGroup(item, addToGroup);
 			}
 			
-			moveToMenu.add(Menu.NONE, Menu.NONE, i, group.getGroupName());
-			++i;
+			selectedItems = null;
 		}
-
-		return true;
 	}
 	
-	@Override
-	public void onItemCheckedStateChanged(ActionMode actionMode, int position, long id, boolean checked) {
-		System.out.println(String.format("Item %d is now %schecked", position, checked ? "": "un"));
-		
-		TodoItem item = (TodoItem) getListView().getItemAtPosition(position);
-		
-		if (null == item) {
-			return;
-		}
-		
-		if (null == selectedItems) {
-			 selectedItems = new ArrayList<TodoItem>();
-		}
-		
-		if (checked) {
-			if (!selectedItems.contains(item)) {
-				selectedItems.add(item);
-			}
-		} else {
-			selectedItems.remove(item);
+	private void emailTodoItems() {
+		if (null != selectedItems) {
+			TodoItemMailer mailer = new TodoItemMailer(getActivity());
+			mailer.addTodoItems(mItem.getGroupName(), selectedItems);
+			mailer.send();
+			selectedItems = null;
 		}
 	}
 }
